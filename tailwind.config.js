69a6b609bf53e7eba201370b238d8016bf0a49f0
wallflower_vy <owoadevic@gmail.com>
2023-03-29 16:22:18 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      lightgreen: "#90DD3E",
+      green: "#183D36",
+      aqua: "#B0FFFC",
+      lightblue: "#DFE5FF",
+      white: "#fff",
+      brown: "#906D6D",
+      blue: "#003DB5",
+      background: "#F3F8FF",
+    },
+
+    fontFamily: {
+      plus: ["Plus Jakarta", "sans-serif"],
+    },
+    extend: {},
+  },
+
+  plugins: [require("tailwindcss"), require("autoprefixer")],
+};
