108be5c3e04d36625668d5370cd193149586164d
wallflower_vy <owoadevic@gmail.com>
2023-03-31 00:08:56 +0100

    vercel fix

--- Components/Button.tsx
+++ Components/Button.tsx
@@ -1,20 +1,13 @@
 import Link from 'next/link'
 import React from 'react';
-import { useRouter } from 'next/router';
 
 
-interface Props {
-   
-   
-  }
+
+
 const Button = () => {
   return (
     <div>
-     {/* <Link {...rest}>
-      <a className={activeClass} onClick={() => router.push(rest.href)}>
-        {text}
-      </a>
-    </Link> */}
+     
     </div>
   )
 }
