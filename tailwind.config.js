c07100ead3bc692354d24ee6b30b5f738f0a594b
wallflower_vy <owoadevic@gmail.com>
2023-03-30 11:42:53 +0100

    minor fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,21 +9,22 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      lightgreen: "#90DD3E",
-      green: "#183D36",
-      aqua: "#B0FFFC",
-      lightblue: "#DFE5FF",
-      white: "#fff",
-      brown: "#906D6D",
-      blue: "#003DB5",
-      background: "#F3F8FF",
-    },
+    extend: {
+      colors: {
+        lightgreen: "#90DD3E",
+        green: "#183D36",
+        aqua: "#B0FFFC",
+        lightblue: "#DFE5FF",
+        white: "#fff",
+        brown: "#906D6D",
+        blue: "#003DB5",
+        background: "#F3F8FF",
+      },
 
-    fontFamily: {
-      plus: ["Plus Jakarta", "sans-serif"],
+      fontFamily: {
+        plus: ["Plus Jakarta", "sans-serif"],
+      },
     },
-    extend: {},
   },
 
   plugins: [require("tailwindcss"), require("autoprefixer")],
