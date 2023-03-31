18c679f388c8bbcc248972cb1b4a0f24458fb1a3
wallflower_vy <owoadevic@gmail.com>
2023-03-31 01:13:51 +0100

    minor fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
